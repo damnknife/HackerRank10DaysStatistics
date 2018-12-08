d269c4746e602462551c0773ff39c7a8f3f19086
damnknife <bruno.apereira93@gmail.com>
2018-12-08 00:13:53 +0000

    Update Day1-0.py

--- Day1-0.py
+++ Day1-0.py
@@ -6,7 +6,7 @@ def mean(Data):
     return sum(Data)/len(Data)
 
 def std(Data):
-    mn = mean(Data)
+    mn = mean(Data) #saving this structure so it only is calculated once
     tmp = 0
     for x in Data:
         tmp += (x-mn)**2
